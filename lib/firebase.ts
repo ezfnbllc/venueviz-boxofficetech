6a5c368461fd67766b481dcc3996acf1311b3905
ezfnbllc <ezfnbllc@gmail.com>
2025-09-26 23:40:15 +0000

    Firebase configured with VenueViz project

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,17 @@
-import{initializeApp}from'firebase/app'
-import{getFirestore}from'firebase/firestore'
-import{getAuth}from'firebase/auth'
-const config={
-  apiKey:"AIzaSyBXyK5rNbOS8wqKkPvMhd5Xm9iQgLqWYmE",
-  authDomain:"venueviz.firebaseapp.com",
-  projectId:"venueviz",
-  storageBucket:"venueviz.appspot.com",
-  messagingSenderId:"123456789",
-  appId:"1:123456789:web:abc123"
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgI8RZe6Ywv3GJvV4-nKc6QMNeoePW9Ao",
+  authDomain: "venueviz.firebaseapp.com",
+  projectId: "venueviz",
+  storageBucket: "venueviz.firebasestorage.app",
+  messagingSenderId: "909590204619",
+  appId: "1:909590204619:web:f38c3e014375e23dac8b4e"
 }
-const app=initializeApp(config)
-export const db=getFirestore(app)
-export const auth=getAuth(app)
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
 export default app
